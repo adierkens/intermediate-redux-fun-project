a6346b903320f2c6f3b60df6d67289f74939388c
Alex Crist <Alex_Crist@intuit.com>
2018-02-07 10:56:45 -0800

    Add state mock

---
+++ src/stateMock.js
@@ -0,0 +1,42 @@
+export default {
+  self: {
+    userId: 1,
+    loggedIn: 1518029635
+  },
+  users: {
+    1: {
+      name: 'BikerBossMarc',
+      lastActivityTime: 1518029645
+    },
+    2: {
+      name: 'ADAMNSon',
+      lastAcitivyTime: 1418029645
+    },
+    3: {
+      name: 'Alex_C_YoungAndSweet_DancingQueen_OnlySeventeen',
+      lastAcitivyTime: 1518024645
+    },
+    4: {
+      name: 'Ravi_FashionablyL8',
+      lastAcitivyTime: 1418029645
+    },
+  },
+  messages: [
+    {
+      messageId: 1,
+      userId: 1,
+      text: 'dfas asdf asdf asdfasda',
+      timeSent: 1518024645
+    }, {
+      messageId: 2,
+      userId: 2,
+      text: 'disc golf rools',
+      timeSent: 1518024646
+    }, {
+      messageId: 3,
+      userId: 4,
+      text: 'aaaaaaaaaahhhhh',
+      timeSent: 1518024647
+    }
+  ]
+};
