8d6b8c6c9ff34934a63b4620ce7fa9c2e88d4c3c
Adam Dierkens <Adam_Dierkens@intuit.com>
2018-02-07 10:16:56 -0800

    Add stuffs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import reducer from './reducers';
+import { createStore } from 'redux';
+
+const store = createStore(reducer)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
